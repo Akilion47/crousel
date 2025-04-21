9a5b1709035834fba83d34e5c0d892102626be66
Nidhi Verma <96578258+nidhirk2020@users.noreply.github.com>
2025-04-21 16:13:56 +0530

    Delete healthbot_360-master directory

--- healthbot_360-master/src/constants/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const backendAPIInstance = axios.create({
-  baseURL: process.env.REACT_APP_HEALTHBOT_AI_API_BASE_URL,
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-    accept: "application/json",
-  },
-});
